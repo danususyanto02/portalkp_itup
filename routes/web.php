306fded91bf1fb4f3b5706e54e23364e0151ddb1
unknown <danu.mds1402@gmail.com>
2021-11-23 13:08:01 +0700

    autentikasi

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/logintest', function () {
+    return view('login');
+});
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
